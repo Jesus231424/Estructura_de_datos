f52a55cd32249f66176cbf946f56b04c90936c05
Jesus231424 <118247729+Jesus231424@users.noreply.github.com>
2023-02-26 13:37:46 -0600

    Add files via upload

---
+++ EVA1_3_HEAP.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_heap;
+
+/**
+ *
+ * @author Marisol G
+ */
+public class EVA1_3_HEAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Prueba miObj = new Prueba ();
+        System.out.println(miObj);
+        Prueba miObj2 = new Prueba ();
+        System.out.println(miObj2);
+       // miObj.x
+    }
+    
+}
+ class Prueba {
+     int x = 100;
+ }
